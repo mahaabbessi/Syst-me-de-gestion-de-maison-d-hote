a7077c1d3b6faa75759b597e956d1e55f906bc9b
Maha Abbessi <mahaabbessi715@gmail.com>
2026-09-08 21:51:52 +0200

    Connect frontend to Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: import.meta.env.VITE_API_URL + "/api",  // Utilisez la variable d'environnement pour l'URL de base
 });
 
 API.interceptors.request.use((req) => {
